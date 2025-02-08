69abd5fbc776809d7f703893c97779b82865d7e8
tangxiaogang <tangxiaogang@apexsoft.com.cn>
2025-02-08 10:31:16 +0800

    update

--- w22_oa_jsp_login_session_10dayfreelogin_jstl/src/com/apexsoft/oa7/action/UserServlet.java
+++ w22_oa_jsp_login_session_10dayfreelogin_jstl/src/com/apexsoft/oa7/action/UserServlet.java
@@ -1,5 +1,6 @@
 package com.apexsoft.oa7.action;
 
+import com.apexsoft.oa7.bean.User;
 import com.apexsoft.oa7.utils.DBUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,10 +14,7 @@ import java.sql.SQLException;
 
 // Servlet负责业务的处理
 // JSP负责页面的展示。
-@WebServlet({
-        "/user/login",
-        "/user/exit"
-})
+@WebServlet({"/user/login","/user/exit"})
 public class UserServlet extends HttpServlet {
 
     @Override
@@ -30,16 +28,49 @@ public class UserServlet extends HttpServlet {
         }
     }
 
-
-    private void doExit(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //获取session
+    protected void doExit(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // 获取session对象，销毁session
         HttpSession session = request.getSession(false);
-        if(session != null){
-            //手动销毁session
+        if (session != null) {
+
+            // 从session域中删除user对象
+            session.removeAttribute("user");
+
+            // 手动销毁session对象。
             session.invalidate();
+
+            // 销毁cookie（退出系统将所有的cookie全部销毁）
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    // 设置cookie的有效期为0，表示删除该cookie
+                    cookie.setMaxAge(0);
+                    // 设置一个下cookie的路径
+                    cookie.setPath(request.getContextPath()); // 删除cookie的时候注意路径问题。
+                    // 响应cookie给浏览器，浏览器端会将之前的cookie覆盖。
+                    response.addCookie(cookie);
+                }
+            }
+
+            // 换一种方案
+            /*Cookie cookie1 = new Cookie("username","");
+            cookie1.setMaxAge(0);
+            cookie1.setPath(request.getContextPath());
+
+            Cookie cookie2 = new Cookie("password", "");
+            cookie2.setMaxAge(0);
+            cookie2.setPath(request.getContextPath());
+
+            response.addCookie(cookie1);
+            response.addCookie(cookie2);*/
+
+
+            // 跳转到登录页面
             response.sendRedirect(request.getContextPath());
         }
     }
+
     protected void doLogin(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         boolean success = false;
@@ -77,8 +108,11 @@ public class UserServlet extends HttpServlet {
         // 登录成功/失败
         if (success) {
             // 获取session对象(这里的要求是：必须获取到session，没有session也要新建一个session对象。)
-            HttpSession session = request.getSession();//session一定不是null
-            session.setAttribute("username", username);
+            HttpSession session = request.getSession(); // session对象一定不是null
+            //session.setAttribute("username", username);
+
+            User user = new User(username, password);
+            session.setAttribute("user", user);
 
             // 登录成功了，并且用户确实选择了“十天内免登录”功能。
             String f = request.getParameter("f");
@@ -104,6 +138,7 @@ public class UserServlet extends HttpServlet {
             // 失败，跳转到失败页面
             response.sendRedirect(request.getContextPath() + "/error.jsp");
         }
+
     }
 }
 
